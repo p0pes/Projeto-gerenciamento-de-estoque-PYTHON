7096b12d7d5a5a3fb002ba892bdaae9fd5376b09
P0pes <peullopes1@gmail.com>
2025-05-08 22:21:47 -0300

    Delete projeto gerenciamento de estoque/main.py

--- projeto gerenciamento de estoque/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-import ui
-from database import inicializar_banco
-
-if __name__ == "__main__":
-    inicializar_banco()
-    root = tk.Tk()
-    app = ui.AplicacaoEstoque(root)
-    root.mainloop()
